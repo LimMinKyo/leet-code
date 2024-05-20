3ddbd11c98682eaddcb6e567efb64a0e381304b3
임민교 <story1400@naver.com>
2024-05-20 17:00:52 +0900

    Time: 0 ms (100%), Space: 2.2 MB (19.59%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.rs
+++ 3379-score-of-a-string/3379-score-of-a-string.rs
@@ -1,5 +1,5 @@
 impl Solution {
     pub fn score_of_string(s: String) -> i32 {
-        return s.as_bytes().windows(2).map(|w| w[0].abs_diff(w[1]) as i32).sum();
+        s.as_bytes().windows(2).map(|w| w[0].abs_diff(w[1]) as i32).sum()
     }
 }
